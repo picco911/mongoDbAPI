86417c99a235900c2b52bef421d54d88062bc43c
Jurijs Moisejevs <jurijs.moisejevs@csc.lv>
2023-10-20 11:52:52 +0300

    bootstrap

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,11 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 import { ModalCreateCVComponent } from './modal-create-cv/modal-create-cv.component';
 import { ModalUpdateCVComponent } from './modal-update-cv/modal-update-cv.component';
 import { ModalDeleteCVConfirmComponent } from './modal-delete-cv-confirm/modal-delete-cv-confirm.component';
 import { ModalViewCVComponent } from './modal-view-cv/modal-view-cv.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -22,6 +23,7 @@ import { ModalViewCVComponent } from './modal-view-cv/modal-view-cv.component';
     HttpClientModule,
     ReactiveFormsModule,
     NgbModule
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
