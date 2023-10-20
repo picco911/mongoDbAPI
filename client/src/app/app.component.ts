cf5e84553fea22d258b4822b68c2961459b22411
Jurijs Moisejevs <jurijs.moisejevs@csc.lv>
2023-10-20 10:25:35 +0300

    lost files

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CV } from './models/cv.model';
+import { CVService } from './services/cv.service';
+import Endpoints from './constants/endpoints';
+import { NgbModal, NgbModalOptions } from '@ng-bootstrap/ng-bootstrap';
+import { ModalCreateCVComponent } from './modal-create-cv/modal-create-cv.component';
+import { ModalUpdateCVComponent } from './modal-update-cv/modal-update-cv.component';
+import { ModalDeleteCVConfirmComponent } from './modal-delete-cv-confirm/modal-delete-cv-confirm.component';
+import { ModalViewCVComponent } from './modal-view-cv/modal-view-cv.component';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Client';
+export class AppComponent implements OnInit{
+  title = 'cvClient';
+
+  modalOptions: NgbModalOptions = {
+    size: 'lg'
+  }
+ 
+  
+
+  constructor(private httpClient: HttpClient, public cvService: CVService, private modalService: NgbModal) { }
+
+  ngOnInit(): void {
+    this.httpClient.get<CV[]>(Endpoints.GET_ALL_POSTS)
+      .subscribe(response => {
+        this.cvService.allPosts = response;
+        console.log(this.cvService.allPosts);
+      })
+  }
+
+  onClickBtnViewPost(post: CV){
+    const modalRef = this.modalService.open(ModalViewCVComponent, this.modalOptions);
+    modalRef.componentInstance.postToUpdate = post;
+  }
+  onClickBtnCreateNewPost(){
+    this.modalService.open(ModalCreateCVComponent)
+  }
+
+   onClickBtnUpdatePost(post: CV){
+     const modalRef = this.modalService.open(ModalUpdateCVComponent, this.modalOptions);
+     modalRef.componentInstance.postToUpdate = post;
+   }
+
+   onClickBtnDeletePost(post: CV) {
+    const modalRef = this.modalService.open(ModalDeleteCVConfirmComponent, this.modalOptions);
+
+    modalRef.componentInstance.postToDelete = post;
+  }
+  
 }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ModalCreateCVComponent } from './modal-create-cv/modal-create-cv.component';
+import { ModalUpdateCVComponent } from './modal-update-cv/modal-update-cv.component';
+import { ModalDeleteCVConfirmComponent } from './modal-delete-cv-confirm/modal-delete-cv-confirm.component';
+import { ModalViewCVComponent } from './modal-view-cv/modal-view-cv.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ModalCreateCVComponent,
+    ModalUpdateCVComponent,
+    ModalDeleteCVConfirmComponent,
+    ModalViewCVComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
